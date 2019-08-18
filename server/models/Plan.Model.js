5b98b0b4a3e60bb909f8415093ca0c6fe9c121f3
ebgester <ebg.ester@gmail.com>
2019-08-18 18:03:31 +0200

    test 2

--- server/models/Plan.Model.js
+++ server/models/Plan.Model.js
@@ -4,8 +4,12 @@ const Schema = mongoose.Schema
 const planSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    inversions: { type: Number, required: true },
-    imageUrl: { type: String, required: true }
+    price: { type: Number, required: true },
+    units:{type:Number, required: true},
+    imageUrl: { type: String, required: true },
+    companyName: {type: String, required: true},
+    address: {type: String, required:true},
+    city: {type: String, required: true}
 }, { timestamps: true })
 
 const Plan = mongoose.model('Plan', planSchema)
