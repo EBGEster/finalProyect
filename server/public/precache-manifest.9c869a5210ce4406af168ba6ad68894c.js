a12be3ced08920fec62c9b16ba706d065b6a57e1
ebgester <ebg.ester@gmail.com>
2019-08-30 11:59:56 +0200

    Deploying

---
+++ server/public/precache-manifest.9c869a5210ce4406af168ba6ad68894c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9e3507d44f519794223eb95e9fd2a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0f1635dd56aa1d916c",
+    "url": "/static/css/main.078494c4.chunk.css"
+  },
+  {
+    "revision": "b1e76435a676610f829e",
+    "url": "/static/js/2.9c219938.chunk.js"
+  },
+  {
+    "revision": "eb0f1635dd56aa1d916c",
+    "url": "/static/js/main.f15284ba.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "ea0d77b71abd4ef0d3de71a0e414c2a9",
+    "url": "/static/media/comment.ea0d77b7.svg"
+  },
+  {
+    "revision": "63098871c62649719f3dc18a26351aa7",
+    "url": "/static/media/fire.63098871.svg"
+  },
+  {
+    "revision": "b8c232a9da0b4b92db71a73408431d64",
+    "url": "/static/media/time.b8c232a9.svg"
+  },
+  {
+    "revision": "2f6a2aceda739a93c988dc19fba293b7",
+    "url": "/static/media/views.2f6a2ace.svg"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b738fda535283c7fc431637741cf8d8.js"
+  "/precache-manifest.9c869a5210ce4406af168ba6ad68894c.js"
 );
 
 self.addEventListener('message', (event) => {
